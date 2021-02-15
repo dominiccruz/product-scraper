b4286025a12f0e497e5dfc163034dc8cef90056a
dominiccruz <79064009+dominiccruz@users.noreply.github.com>
2021-02-15 12:06:52 +0000

    Initial project setup generated from https://start.spring.io

---
+++ src/main/java/com/jsainsburys/productscraperservice/ProductScraperServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jsainsburys.productscraperservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductScraperServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductScraperServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsainsburys/productscraperservice/ProductScraperServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsainsburys.productscraperservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductScraperServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
